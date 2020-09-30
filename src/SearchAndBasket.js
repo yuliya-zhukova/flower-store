82c08d026986fe908a75c3619aa2e5659c88e041
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-09-30 13:44:44 +0200

    Fix images with alt

--- src/SearchAndBasket.js
+++ src/SearchAndBasket.js
@@ -8,10 +8,10 @@ export function SearchAndBasket() {
   return (
       <div className="searchAndBasket">
           <a href="#" className="searchAndBasket__loop">
-              <img src={loop}></img>
+              <img src={loop} alt="Search"></img>
           </a>
           <a href="#" className="searchAndBasket__basket">
-              <img src={basket}></img>
+              <img src={basket} alt="Basket"></img>
           </a>
       </div>
   );
