12f202b676323af5ee96f2c8c0b22fe754b94ebb
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-09-30 10:53:57 +0200

    Add the location modul

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './header.scss';
 
+import { Location } from './Location';
+
 export function Header() {
   return (
       <div className="header">
-        <div className="header__left">Location</div>
+        <Location />
         <h1 className="header__center">Cassiopeia</h1>
         <div className="header__right">Basket</div>
       </div>
