f3bd40c4340ca5643def1c3a8c3095c2b724bdae
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-10-01 16:47:56 +0200

    Rename image

--- src/SearchAndBasket.js
+++ src/SearchAndBasket.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './searchAndBasket.scss';
 
-import loop from './loop.png';
+import search from './search.png';
 import basket from './basket.png';
 
 export function SearchAndBasket() {
   return (
       <div className="searchAndBasket">
           <a href="#" className="searchAndBasket__loop">
-              <img src={loop} alt="Search"></img>
+              <img src={search} alt="Search"></img>
           </a>
           <a href="#" className="searchAndBasket__basket">
               <img src={basket} alt="Basket"></img>
