ae56f10c0b19a06a89cb02541106f3bc94ec82e6
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-10-26 16:08:22 +0100

    Add a header for the section

--- src/sections/Sections.js
+++ src/sections/Sections.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./sections.scss";
+import "./Sections.scss";
 
 import { Products } from "../products/Products";
 import { sectionList } from "./sectionList";
@@ -9,10 +9,20 @@ export function Sections() {
     <div className="section">
       {sectionList.map(function (section, i) {
           return (
-            <div key={i}>
-              <h3 className="section__title">
-                {section.title}
-              </h3>
+            <div key={i} className="section__slider">
+              <div className="section__header">
+                <h3 className="section__title">
+                  {section.title}
+                </h3>
+                <div className="section__navigation">
+                  <a href="/" className="section__nav-item section__nav-item_disable">
+                    <div className="section__direction_left"></div>
+                  </a>
+                  <a href="/" className="section__nav-item">
+                    <div className="section__direction_right"></div>
+                  </a>
+                </div>
+              </div>
               <Products />
             </div>
           );
