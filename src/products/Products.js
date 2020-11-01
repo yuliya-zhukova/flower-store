797d58f79efa5161831741669882177b3d4e6f41
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-11-01 23:18:00 +0100

    Refactor layout for the slider

--- src/products/Products.js
+++ src/products/Products.js
@@ -16,33 +16,37 @@ function toWishList () {
 
 export function Products() {
   return (
-    <div className="product__row">
-      {productList.map(function (product, i) {
-        return (
-          <div className="product__item" key={i}>
-            <div className="product__image">
-              <img src={image_small} alt="New"></img>
-              <div className="product__button-block">
-                <Button classNames={"button button__product button__product_to-basket"} clickEvent={toBasket}/>
-                <Button classNames={"button button__product button__product_like"} clickEvent={toWishList} />
+    <div className="product__window">
+      <div className="product__row">
+        {productList.map(function (product, i) {
+          return (
+            <div className="product__row-item">
+              <div className="product__item" key={i}>
+                <div className="product__image">
+                  <img src={image_small} alt="New"></img>
+                  <div className="product__button-block">
+                    <Button classNames={"button button__product button__product_to-basket"} clickEvent={toBasket}/>
+                    <Button classNames={"button button__product button__product_like"} clickEvent={toWishList} />
+                  </div>
+                </div>
+                <div className="product__item-info">
+                  <h4 className="product__item-title">{product.title}</h4>
+                  <div className="product__price-row">
+                    <div className="product__price">{product.price}</div>
+                    {
+                      product.oldPrice ? (
+                      <div className="product__price product__price_old">{product.oldPrice}</div>
+                      ) : (
+                        <></>
+                      )
+                    }
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="product__item-info">
-              <h4 className="product__item-title">{product.title}</h4>
-              <div className="product__price-row">
-                <div className="product__price">{product.price}</div>
-                {
-                  product.oldPrice ? (
-                  <div className="product__price product__price_old">{product.oldPrice}</div>
-                  ) : (
-                    <></>
-                  )
-                }
-              </div>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
