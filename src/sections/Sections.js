f8f64c67ab124097c1a4409772333711799f4e56
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-11-02 12:50:00 +0100

    Handle button navigation clicks

--- src/sections/Sections.js
+++ src/sections/Sections.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Sections.scss";
 
+import { Button } from "../button/Button";
+import { productList } from "../products/productList";
 import { Products } from "../products/Products";
 import { sectionList } from "./sectionList";
+
+let clickMoveLeft = 300;
+let clickMoveRight = 0;
+let productRowLength = productList.length;
+
+function pressButtonLeft () {
+  document.querySelector('.product__row').style.left = "-" + clickMoveLeft + "px";
+  if ((clickMoveLeft / 300) <= (productRowLength - 4)) {
+    clickMoveLeft = clickMoveLeft + 300;
+    clickMoveRight = clickMoveRight - clickMoveLeft;
+  } else {
+    document.querySelector('.section__nav-item').classList.add('section__nav-item_disable');
+  }
+}
+
+function pressButtonRight () {
+  document.querySelector('.product__row').style.left = clickMoveRight + "px";
+  clickMoveRight = clickMoveRight + 300;
+}
 
 export function Sections() {
   return (
@@ -15,12 +36,8 @@ export function Sections() {
                   {section.title}
                 </h3>
                 <div className="section__navigation">
-                  <a href="/" className="section__nav-item section__nav-item_disable">
-                    <div className="section__direction_left"></div>
-                  </a>
-                  <a href="/" className="section__nav-item">
-                    <div className="section__direction_right"></div>
-                  </a>
+                  <Button title={"←"} classNames={"section__nav-item section__nav-item_left"} clickEvent={pressButtonLeft} />
+                  <Button title={"→"} classNames={"section__nav-item section__nav-item_right"} clickEvent={pressButtonRight}/>
                 </div>
               </div>
               <Products />
