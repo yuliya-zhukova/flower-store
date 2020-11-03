90b86edbb789eedae4f3e23527df450aa8f6b257
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-11-03 15:49:14 +0100

    Add a test data

--- src/compositions/content/Content.js
+++ src/compositions/content/Content.js
@@ -5,12 +5,14 @@ import { Primary } from "../../components/primary/Primary";
 import { Regular } from "../../components/regular/Regular";
 import { Carousel } from "../../components/Carousel/Carousel";
 import { carousel } from "../../demoData/carousel";
+import { carouselNew } from "../../demoData/carouselNew";
 
 export function Content() {
   return (
     <div className="content">
       <Primary />
       <Regular />
+      <Carousel title={carouselNew.title} items={carouselNew.items} />
       <Carousel title={carousel.title} items={carousel.items} />
     </div>
   );
---
+++ src/demoData/carouselNew.js
@@ -0,0 +1,43 @@
+export const carouselNew = {
+    title: "New",
+    items: [
+        {
+            image: "/images/image_small.png",
+            name: "White roses, 7pc.",
+            price: "44 $",
+        },
+        {
+            image: "/images/image_small.png",
+            name: "White roses, 7pc.",
+            price: "44 $",
+        },
+        {
+            image: "/images/image_small.png",
+            name: "Red roses, 5pc.",
+            price: "20 $",
+            oldPrice: "99 $"
+        },
+        {
+            image: "/images/image_small.png",
+            name: "White lilies, 15pc.",
+            price: "70 $",
+        },
+        {
+            image: "/images/image_small.png",
+            name: "Yellow roses, 25pc.",
+            price: "100 $",
+            oldPrice: "150 $"
+        },
+        {
+            image: "/images/image_small.png",
+            name: 'Bouquet "Sunny"',
+            price: "15 $",
+        },
+        {
+            image: "/images/image_small.png",
+            name: "Pink roses, 3pc.",
+            price: "3 $",
+            oldPrice: "15 $"
+        }
+    ]
+}
