c6c80f9503d3d64c3f2b860c65dd951b6e15555a
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-11-03 22:22:17 +0100

    Refactor Carousel component with class

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,35 +4,45 @@ import "./Carousel.scss";
 import { Button } from "../button/Button";
 import { CarouselItem } from "./CarouselItem/CarouselItem";
 
-export function Carousel(props) {
-  return (
-    <div className="section">
-      <div className="section__slider">
-        <div className="section__header">
-          <h3 className="section__title">
-            {props.title}
-          </h3>
-          <div className="section__navigation">
-            <Button title={"←"} classNames={"section__nav-item section__nav-item_left"} />
-            <Button title={"→"} classNames={"section__nav-item section__nav-item_right"} />
+export class Carousel extends React.Component {
+  handleButtonClickLeft () {
+    console.log('Left')
+  }
+
+  handleButtonClickRight () {
+    console.log('Right')
+  }
+
+  render() {
+    return (
+      <div className="section">
+        <div className="section__slider">
+          <div className="section__header">
+            <h3 className="section__title">
+              {this.props.title} - {this.carouselType}
+            </h3>
+            <div className="section__navigation">
+              <Button title={"←"} clickEvent={this.handleButtonClickLeft} classNames={"section__nav-item section__nav-item_left"} />
+              <Button title={"→"} clickEvent={this.handleButtonClickRight}classNames={"section__nav-item section__nav-item_right"} />
+            </div>
           </div>
-        </div>
-        <div className="product__window">
-          <div className="product__row">
-            {props.items.map(function (item, i) {
-              return (
-                <CarouselItem
-                image={item.image}
-                name={item.name}
-                price={item.price}
-                oldPrice={item.oldPrice}
-                key={i}
-                />
-              );
-            })}
+          <div className="product__window">
+            <div className="product__row">
+              {this.props.items.map(function (item, i) {
+                return (
+                  <CarouselItem
+                    image={item.image}
+                    name={item.name}
+                    price={item.price}
+                    oldPrice={item.oldPrice}
+                    key={i}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
