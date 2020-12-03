2d13649fa24e34d631e91dbd96059dabb7033f1d
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-12-03 11:33:28 +0100

    Add items for accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import './Accordion.scss';
 
+const accordionList = [
+  {
+    id: 'item1',
+    title: 'Details1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elitsed.'
+  },
+  {
+    id: 'item2',
+    title: 'Details2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elitsed.'
+  },
+  {
+    id: 'item3',
+    title: 'Details3',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elitsed.'
+  }
+];
+
 export function Accordion (): JSX.Element {
   return (
     <div className="accordion">
-      <div className="accordion__container">
-        <input
-          id="item1"
-          type="checkbox"
-          className="accordion__input"
-        />
-        <label
-          htmlFor="item1"
-          className="accordion__container-header"
-        >
-          Details
-        </label>
-        <div className="accordion__container-content">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      {accordionList.map((item: { id: string; title: string; content: string; }): JSX.Element => (
+        <div key={item.id} className="accordion__container">
+          <input
+            id={item.id}
+            type="checkbox"
+            className="accordion__input"
+          />
+          <label
+            htmlFor={item.id}
+            className="accordion__container-header"
+          >
+            {item.title}
+          </label>
+          <div className="accordion__container-content">
+              {item.content}
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
