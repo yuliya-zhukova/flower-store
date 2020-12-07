01ea2e3f159ef36e5c2ffc93255ed500bafb1803
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-12-07 19:02:53 +0100

    Remove the link from the last list item

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -20,12 +20,18 @@ export function Breadcrumbs (): JSX.Element {
   return (
     <div className="breadcrumbs">
       <ul className="breadcrumbs__list">
-        {breadcrumbs.map((item: { href: string; name: string; }): JSX.Element => {
+        {breadcrumbs.map((item: { href: string; name: string; }, id: number): JSX.Element => {
           return (
             <li key={item.name} className="breadcrumbs__item">
-              <a href={item.href} className="breadcrumbs__link">
-                {item.name}
-              </a>
+              {
+                id === breadcrumbs.length - 1 ? (
+                  <span>{item.name}</span>
+                ) : (
+                  <a href={item.href} className="breadcrumbs__link">
+                    {item.name}
+                  </a>
+                )
+              }
             </li>
           );
         })}
