32d2db81a13078c5cc633ecacaf773a5d004c9e1
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-12-07 12:10:28 +0100

    Add styles for Breadcrumbs component

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Breadcrumbs.scss';
 
+const breadcrumbs = [
+  {
+    href: '#',
+    name: 'Level 1'
+  },
+  {
+    href: '#',
+    name: 'Level 2'
+  },
+  {
+    href: '#',
+    name: 'Level 3'
+  }
+];
+
 export function Breadcrumbs (): JSX.Element {
   return (
     <div className="breadcrumbs">
-      Breadcrumbs
+      <ul className="breadcrumbs__list">
+        {breadcrumbs.map((item: { href: string; name: string; }): JSX.Element => {
+          return (
+            <li key={item.name} className="breadcrumbs__item">
+              <a href={item.href} className="breadcrumbs__link">
+                {item.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
